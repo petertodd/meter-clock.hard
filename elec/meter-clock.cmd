6978030ae9e7c95244a74d9fe1358474107362e2
Peter Todd <pete@petertodd.org>
2008-06-13 23:47:44 -0400

    Ran gsch2pcb

---
+++ elec/meter-clock.cmd
@@ -0,0 +1,236 @@
+# Pin name action command file
+
+# Start of element C602
+ChangePinName(C602, 2, 2)
+ChangePinName(C602, 1, 1)
+
+# Start of element C601
+ChangePinName(C601, 2, 2)
+ChangePinName(C601, 1, 1)
+
+# Start of element U401
+ChangePinName(U401, 10, RB4/AN6/RX/DT/KBI0)
+ChangePinName(U401, 11, RB5/PGM/KBI1)
+ChangePinName(U401, 12, RB6/PGC/T1OSO/T13CKI)
+ChangePinName(U401, 13, RB7/PGD/T1OSI)
+ChangePinName(U401, 14, Vdd)
+ChangePinName(U401, 15, RA6/OSC2/CLKO)
+ChangePinName(U401, 16, RA7/OSC1/CLKI)
+ChangePinName(U401, 17, RB2/P1B/INT2)
+ChangePinName(U401, 18, RB3/CCP1/P1A)
+ChangePinName(U401, 9, RB1/AN5/TX/CK/INT1)
+ChangePinName(U401, 8, RB0/AN4/INT0)
+ChangePinName(U401, 7, RA3/AN3/VREF+)
+ChangePinName(U401, 6, RA2/AN2/VREF-)
+ChangePinName(U401, 5, Vss)
+ChangePinName(U401, 4, MCLR/RA5/Vpp)
+ChangePinName(U401, 3, RA4/T0CKI)
+ChangePinName(U401, 2, RA1/AN1/LVDIN)
+ChangePinName(U401, 1, RA0/AN0)
+
+# Start of element U601
+ChangePinName(U601, 4, 4)
+ChangePinName(U601, 3, 3)
+ChangePinName(U601, 2, 2)
+ChangePinName(U601, 1, 1)
+
+# Start of element ICSP301
+ChangePinName(ICSP301, 6, 6)
+ChangePinName(ICSP301, 5, 5)
+ChangePinName(ICSP301, 4, 4)
+ChangePinName(ICSP301, 3, 3)
+ChangePinName(ICSP301, 2, 2)
+ChangePinName(ICSP301, 1, 1)
+
+# Start of element CONN601
+ChangePinName(CONN601, 2, 2)
+ChangePinName(CONN601, 1, 1)
+
+# Start of element TP105
+ChangePinName(TP105, 1, 1)
+
+# Start of element S803
+ChangePinName(S803, 2, 2)
+ChangePinName(S803, 1, 1)
+
+# Start of element R102
+ChangePinName(R102, 1, 1)
+ChangePinName(R102, 2, 2)
+
+# Start of element R203
+ChangePinName(R203, 1, 1)
+ChangePinName(R203, 2, 2)
+
+# Start of element R506
+ChangePinName(R506, 1, 1)
+ChangePinName(R506, 3, 3)
+ChangePinName(R506, 2, 2)
+
+# Start of element TP104
+ChangePinName(TP104, 1, 1)
+
+# Start of element S802
+ChangePinName(S802, 2, 2)
+ChangePinName(S802, 1, 1)
+
+# Start of element R202
+ChangePinName(R202, 1, 1)
+ChangePinName(R202, 2, 2)
+
+# Start of element R505
+ChangePinName(R505, 1, 1)
+ChangePinName(R505, 3, 3)
+ChangePinName(R505, 2, 2)
+
+# Start of element TP103
+ChangePinName(TP103, 1, 1)
+
+# Start of element R101
+ChangePinName(R101, 1, 1)
+ChangePinName(R101, 2, 2)
+
+# Start of element S801
+ChangePinName(S801, 2, 2)
+ChangePinName(S801, 1, 1)
+
+# Start of element R201
+ChangePinName(R201, 1, 1)
+ChangePinName(R201, 2, 2)
+
+# Start of element TP506
+ChangePinName(TP506, 1, 1)
+
+# Start of element R504
+ChangePinName(R504, 1, 1)
+ChangePinName(R504, 3, 3)
+ChangePinName(R504, 2, 2)
+
+# Start of element TP102
+ChangePinName(TP102, 1, 1)
+
+# Start of element R302
+ChangePinName(R302, 1, 1)
+ChangePinName(R302, 2, 2)
+
+# Start of element TP202
+ChangePinName(TP202, 1, 1)
+
+# Start of element TP505
+ChangePinName(TP505, 1, 1)
+
+# Start of element R503
+ChangePinName(R503, 1, 1)
+ChangePinName(R503, 3, 3)
+ChangePinName(R503, 2, 2)
+
+# Start of element TP101
+ChangePinName(TP101, 1, 1)
+
+# Start of element R301
+ChangePinName(R301, 1, 1)
+ChangePinName(R301, 2, 2)
+
+# Start of element TP403
+ChangePinName(TP403, 1, 1)
+
+# Start of element TP504
+ChangePinName(TP504, 1, 1)
+
+# Start of element R502
+ChangePinName(R502, 1, 1)
+ChangePinName(R502, 3, 3)
+ChangePinName(R502, 2, 2)
+
+# Start of element TP201
+ChangePinName(TP201, 1, 1)
+
+# Start of element TP402
+ChangePinName(TP402, 1, 1)
+
+# Start of element TP503
+ChangePinName(TP503, 1, 1)
+
+# Start of element TP301
+ChangePinName(TP301, 1, 1)
+
+# Start of element R501
+ChangePinName(R501, 1, 1)
+ChangePinName(R501, 3, 3)
+ChangePinName(R501, 2, 2)
+
+# Start of element R803
+ChangePinName(R803, 1, 1)
+ChangePinName(R803, 2, 2)
+
+# Start of element R601
+ChangePinName(R601, 1, 1)
+ChangePinName(R601, 2, 2)
+
+# Start of element TP401
+ChangePinName(TP401, 1, 1)
+
+# Start of element TP502
+ChangePinName(TP502, 1, 1)
+
+# Start of element R802
+ChangePinName(R802, 1, 1)
+ChangePinName(R802, 2, 2)
+
+# Start of element TP602
+ChangePinName(TP602, 1, 1)
+
+# Start of element TP501
+ChangePinName(TP501, 1, 1)
+
+# Start of element R801
+ChangePinName(R801, 1, 1)
+ChangePinName(R801, 2, 2)
+
+# Start of element TP601
+ChangePinName(TP601, 1, 1)
+
+# Start of element C103
+ChangePinName(C103, 2, 2)
+ChangePinName(C103, 1, 1)
+
+# Start of element D601
+ChangePinName(D601, 1, 1)
+ChangePinName(D601, 2, 2)
+
+# Start of element C102
+ChangePinName(C102, 2, 2)
+ChangePinName(C102, 1, 1)
+
+# Start of element C101
+ChangePinName(C101, 2, -)
+ChangePinName(C101, 1, +)
+
+# Start of element U102
+ChangePinName(U102, 1, 1)
+ChangePinName(U102, 2, 2)
+
+# Start of element C603
+ChangePinName(C603, 2, 2)
+ChangePinName(C603, 1, 1)
+
+# Start of element C401
+ChangePinName(C401, 2, 2)
+ChangePinName(C401, 1, 1)
+
+# Start of element U101
+ChangePinName(U101, unknown, unknown)
+ChangePinName(U101, unknown, unknown)
+ChangePinName(U101, unknown, unknown)
+ChangePinName(U101, unknown, unknown)
+ChangePinName(U101, unknown, unknown)
+ChangePinName(U101, unknown, unknown)
+ChangePinName(U101, unknown, unknown)
+ChangePinName(U101, unknown, unknown)
+ChangePinName(U101, unknown, unknown)
+ChangePinName(U101, 15, SDA)
+ChangePinName(U101, 16, SCL)
+ChangePinName(U101, 4, RST)
+ChangePinName(U101, 3, INT/SQW)
+ChangePinName(U101, 2, Vcc)
+ChangePinName(U101, 1, 32kHz)
+ChangePinName(U101, 14, Vbat)
